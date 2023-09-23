54acf79808d0631b1ae443010e664c6773f810f6
Rcatarata <Rcatarata@gmail.com>
2023-09-23 14:56:26 -0400

    facts
    facts

--- potatoe/app/api/facts/route.ts
+++ potatoe/app/api/facts/route.ts
@@ -1,87 +1,112 @@
 import { getServerSession } from "next-auth";
 import {NextResponse} from 'next/server'
+import Potato1 from '../../../public/assets/russet.webp'
+import Andes from '../../../public/assets/facts/andes.jpg'
+import PotVar from '../../../public/assets/facts/potatovar.jpeg'
+import Chips from '../../../public/assets/facts/chips.jpg'
+import Space from '../../../public/assets/facts/space.jpg'
+import Mashed from '../../../public/assets/facts/mashed.jpg'
+import PotEat from '../../../public/assets/facts/potatoeatters.jpg'
+import Famine from '../../../public/assets/facts/famine.jpg'
+import PotRoom from '../../../public/assets/facts/potatoroom.jpeg'
+import PotLight from '../../../public/assets/facts/potatoLight.jpg'
+import Andes2 from '../../../public/assets/facts/andes2.avif'
+import LargePot from '../../../public/assets/facts/largepot.jpeg'
+import PotHeart from '../../../public/assets/facts/potheart.jpg'
+import PotKnife from '../../../public/assets/facts/potknife.jpg'
+import { StaticImageData } from 'next/image';
 
 interface PotatoFacts {
     id: number, 
     title: string, 
     fact: string
+    img: StaticImageData
 }
 
 const potatoFacts : PotatoFacts[] = [
     {
       id: 1,
       title: "Origin",
+      img: Andes, 
       fact: "Potatoes were first cultivated by indigenous peoples in the Andes Mountains of South America over 7,000 years ago. They have been a staple food in the region ever since.",
     },
     {
       id: 2,
       title: "Variety",
+      img: PotVar,
       fact: "There are thousands of potato varieties, but they can be broadly categorized into five main types: russet, red, white, yellow, and fingerling.",
     },
     {
       id: 3,
       title: "Nutritional Value",
+      img: PotKnife,
       fact: "Potatoes are a good source of vitamins (particularly vitamin C and B6), minerals (like potassium), and dietary fiber. They are also fat-free and relatively low in calories, making them a healthy addition to meals.",
     },
     {
       id: 4,
       title: "World's Largest Potato",
+      img: LargePot,
       fact: "The largest potato ever grown weighed in at 18 pounds and 4 ounces, according to the Guinness World Records.",
     },
     {
       id: 5,
       title: "Potato Chip Origin",
+      img: Chips,
       fact: "The potato chip, a popular snack, was invented by chef George Crum in 1853 at Moon's Lake House in Saratoga Springs, New York. He made them as a joke, but they became a sensation.",
     },
     {
       id: 6,
       title: "Potatoes in Space",
+      img: Space,
       fact: "Potatoes have been grown in space. In 2015, NASA successfully cultivated potatoes on the International Space Station as part of an experiment to study food production in microgravity.",
     },
     {
       id: 7,
       title: "Cultural Significance",
+      img: Mashed,
       fact: "Potatoes have played a crucial role in various cultures' cuisines. For example, they are a fundamental ingredient in dishes like French fries, mashed potatoes, and Irish colcannon.",
     },
     {
       id: 8,
       title: "Irish Potato Famine",
+      img: Famine,
       fact: "The Irish Potato Famine, which occurred between 1845 and 1852, was a devastating period in Irish history when a potato disease called late blight wiped out the potato crop. This led to a significant loss of life and mass emigration from Ireland.",
     },
     {
       id: 9,
       title: "Potatoes and the Andes",
+      img: Andes2,
       fact: "In the Andean region, there are over 4,000 different varieties of potatoes, showcasing their incredible genetic diversity.",
     },
     {
       id: 10,
       title: "Biodiversity Treasure",
+      img: Potato1,
       fact: "Potatoes are considered one of the world's most important crops, providing sustenance for billions of people. Their genetic diversity is a crucial resource for developing new varieties that are resistant to diseases and pests.",
     },
     {
       id: 11,
       title: "Potato as an Energy Source",
+      img: PotLight,
       fact: "Potatoes can be used to produce electricity. The chemicals in potatoes, when combined with other materials, can create a simple battery.",
     },
     {
       id: 12,
       title: "Not Just for Eating",
+      img: PotHeart,
       fact: "Potatoes have been used for more than just food. They have been employed in folk remedies for ailments like burns, headaches, and warts. They can also be used for cleaning and polishing metals.",
     },
     {
       id: 13,
       title: "Potatoes in Art",
+      img: PotEat,
       fact: "The famous artist Vincent van Gogh created a still-life painting titled 'The Potato Eaters' in 1885. It depicts a family of Dutch peasants sitting down for a meal of potatoes.",
     },
     {
       id: 14,
       title: "Long Shelf Life",
+      img: PotRoom,
       fact: "When stored in a cool, dark place, potatoes can have a relatively long shelf life. They can stay fresh for several months, making them an excellent option for food storage.",
-    },
-    {
-      id: 15,
-      title: "Potato Diplomacy",
-      fact: "In 1974, during the Cold War, potatoes played a unique diplomatic role when the United States and the Soviet Union exchanged potato-themed gifts as a symbol of détente and peaceful relations.",
     },
   ];
   
--- potatoe/app/facts/page.tsx
+++ potatoe/app/facts/page.tsx
@@ -1,20 +1,72 @@
+// 'use client'
+// import { useEffect, useState } from "react"
+// import {motion, useScroll, useTransform} from 'framer-motion'
+// import {useRef} from 'react'
+
+
+// function PotatoFacts() {
+//   const ref = useRef(null)
+//   // const {scrollYProgress} = useScroll({
+//   //   target: ref, 
+//   //   offset: ['0 1', '1.1, 1']
+//   // })
+
+//   // const x1 = useTransform(scrollYProgress, [0,1], [-1000, 0])
+//   // const x2 = useTransform(scrollYProgress, [0,1], [1000, 0])
+
+//   const [facts, setFacts] = useState([])
+
+//   useEffect(() => {
+//     fetch('http://localhost:3000/api/facts')
+//     .then((res) => res.json())
+//     .then((data) => setFacts(data))
+
+//   }, [])
+  
+
+//   return (
+//     <div className="w-screen">
+//         <h1 className='text-3xl'> Potatoes are fascinating tubers with a rich history and many interesting facts. Here are some of the best potato facts:</h1>
+//         <div className="w-full flex flex-col justify-center items-center">
+//           {facts? 
+//             facts.slice(1).map((fact : any) => {
+//               if (fact.id % 2 === 0) {
+//                 return (
+//                   <motion.div whileInView={{y: -10, opacity: 1}} transition={{ ease: "easeOut", duration: 1 }} initial={{opacity: 0}} className="h-auto bg-stone-400 w-[500px] p-10 my-4">
+//                     <h1 className="text-3xl">{fact.title}</h1>
+//                     <p className="text-lg">{fact.fact}</p>
+//                   </motion.div>
+//                 )
+//               } else  {
+//                 return (
+//                   <motion.div whileInView={{y: -10, opacity: 1}} transition={{ ease: "easeOut", duration: 1 }} initial={{opacity: 0}} className="h-auto bg-indigo-300 w-[500px] p-10 my-4">
+//                     <h1 className="text-3xl">{fact.title}</h1>
+//                     <p className="text-lg">{fact.fact}</p>
+//                   </motion.div>
+//                 )
+//               }
+//             }
+//             )
+//           :''}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default PotatoFacts
 'use client'
-import { useEffect, useState } from "react"
-import {motion, useScroll, useTransform} from 'framer-motion'
-import {useRef} from 'react'
+import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
+import { FiArrowDown, FiArrowUp } from "react-icons/fi";
+import { useRef, useEffect, useState } from "react";
+import Image from 'next/image'
 
+const OppoScroll = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
 
-function PotatoFacts() {
-  const ref = useRef(null)
-  // const {scrollYProgress} = useScroll({
-  //   target: ref, 
-  //   offset: ['0 1', '1.1, 1']
-  // })
-
-  // const x1 = useTransform(scrollYProgress, [0,1], [-1000, 0])
-  // const x2 = useTransform(scrollYProgress, [0,1], [1000, 0])
-
-  const [facts, setFacts] = useState([])
+    const [facts, setFacts] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:3000/api/facts')
@@ -22,35 +74,99 @@ function PotatoFacts() {
     .then((data) => setFacts(data))
 
   }, [])
-  
 
   return (
-    <div className="w-screen">
-        <h1 className='text-3xl'> Potatoes are fascinating tubers with a rich history and many interesting facts. Here are some of the best potato facts:</h1>
-        <div className="w-full flex flex-col justify-center items-center">
-          {facts? 
-            facts.slice(1).map((fact : any) => {
-              if (fact.id % 2 === 0) {
-                return (
-                  <motion.div whileInView={{y: -10, opacity: 1}} transition={{ ease: "easeOut", duration: 1 }} initial={{opacity: 0}} className="h-auto bg-stone-400 w-[500px] p-10 my-4">
-                    <h1 className="text-3xl">{fact.title}</h1>
-                    <p className="text-lg">{fact.fact}</p>
-                  </motion.div>
-                )
-              } else  {
-                return (
-                  <motion.div whileInView={{y: -10, opacity: 1}} transition={{ ease: "easeOut", duration: 1 }} initial={{opacity: 0}} className="h-auto bg-indigo-300 w-[500px] p-10 my-4">
-                    <h1 className="text-3xl">{fact.title}</h1>
-                    <p className="text-lg">{fact.fact}</p>
-                  </motion.div>
-                )
-              }
-            }
-            )
-          :''}
-        </div>
-    </div>
-  )
-}
+    <>
+      {/* <img src={facts[0].img} alt="" /> */}
+      <div className="bg-white text-black p-4 grid place-items-center">
+        <FiArrowDown className="text-xl" />
+      </div>
+      <section ref={targetRef} className="flex bg-black text-white">
+        <Content content={facts} />
+        <Images content={facts} scrollYProgress={scrollYProgress} />
+      </section>
+      <div className="bg-black text-white p-4 grid place-items-center">
+        <FiArrowUp className="text-xl" />
+      </div>
+    </>
+  );
+};
 
-export default PotatoFacts
+const Content = ({ content }) => {
+  return (
+    <div className="w-full">
+      {content.map(({ id, title, fact }, idx) => (
+        <div
+          key={id}
+          className={`p-8 h-screen flex flex-col justify-between ${
+            idx % 2 ? "bg-white text-black" : "bg-black text-white"
+          }`}
+        >
+          <h3 className="text-3xl font-medium">{title}</h3>
+          <p className="font-light w-full max-w-md">{fact}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Images = ({ content, scrollYProgress }) => {
+  const top = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [`-${(content.length - 1) * 100}vh`, "0vh"]
+  );
+
+  return (
+    <div className="h-screen overflow-hidden sticky top-0 w-24 md:w-full">
+      <motion.div style={{ top }} className="absolute left-0 right-0">
+        {[...content].reverse().map(({ img, id, title }) => (
+          // <img
+            // key={id}
+            // alt={title}
+            // className="h-screen w-full object-cover"
+            // src={img}
+          // />
+          <Image
+            src={img}
+            className="h-screen w-full object-cover"
+            alt={title}
+          />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default OppoScroll;
+
+const items = [
+  {
+    id: 1,
+    title: "Black Nike w white check",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    img: "https://images.unsplash.com/photo-1543508282-6319a3e2621f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=830&q=80",
+  },
+  {
+    id: 2,
+    title: "Bunch of black and white shoes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    img: "https://images.unsplash.com/photo-1600054904350-1d493ae5f922?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=930&q=80",
+  },
+  {
+    id: 3,
+    title: "White shoes in the rain",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    img: "https://images.unsplash.com/photo-1465479423260-c4afc24172c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80",
+  },
+  {
+    id: 4,
+    title: "White shoes with black heel",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    img: "https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1662&q=80",
+  },
+];
