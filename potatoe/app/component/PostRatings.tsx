46e71f021e6a9cee8ecf116a8122ef6cddaa7f6c
Rcatarata <Rcatarata@gmail.com>
2023-10-05 21:47:22 -0400

    rating ui

--- potatoe/app/component/PostRatings.tsx
+++ potatoe/app/component/PostRatings.tsx
@@ -1,9 +1,24 @@
+import moment from "moment"
+import {BsStarFill, BsStar } from 'react-icons/bs'
+
+
 function PostRatings({post}) {
+  
   return (
-    <div>
-      <p>{post.user.name}</p>
-      <p>{post.rating}</p>
+    <div className="w-96">
+       <div className="flex flex-row"> 
+        {post.rating >= 1 ? <BsStarFill/>:<BsStar/>}
+        {post.rating >= 2 ? <BsStarFill/>:<BsStar/>}
+        {post.rating >= 3 ? <BsStarFill/>:<BsStar/>}
+        {post.rating >= 4 ? <BsStarFill/>:<BsStar/>}
+        {post.rating >= 5 ? <BsStarFill/>:<BsStar/>}
+      </div>
       <h1>{post.post}</h1>
+      <div className="flex justify-between">
+        <p>{post.user.name.split(' ')[0]}</p> 
+        <p>{moment(post.createdAt).format('LL')}</p>
+      </div>
+      {/* <p>{post.rating}</p> */}
     </div>
   )
 }
