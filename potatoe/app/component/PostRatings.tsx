8d35290687ed08994c0550c21a26ed4e3d20b5f7
Rcatarata <Rcatarata@gmail.com>
2023-10-06 17:57:42 -0400

    ui rating post

--- potatoe/app/component/PostRatings.tsx
+++ potatoe/app/component/PostRatings.tsx
@@ -5,13 +5,13 @@ import {BsStarFill, BsStar } from 'react-icons/bs'
 function PostRatings({post}) {
   
   return (
-    <div className="w-96">
+    <div className="w-96 h-24 border">
        <div className="flex flex-row"> 
-        {post.rating >= 1 ? <BsStarFill/>:<BsStar/>}
-        {post.rating >= 2 ? <BsStarFill/>:<BsStar/>}
-        {post.rating >= 3 ? <BsStarFill/>:<BsStar/>}
-        {post.rating >= 4 ? <BsStarFill/>:<BsStar/>}
-        {post.rating >= 5 ? <BsStarFill/>:<BsStar/>}
+        {post.rating >= 1 ? <BsStarFill className='fill-amber-400'/>:<BsStar className='fill-amber-500'/>}
+        {post.rating >= 2 ? <BsStarFill className='fill-amber-400'/>:<BsStar className='fill-amber-500'/>}
+        {post.rating >= 3 ? <BsStarFill className='fill-amber-400'/>:<BsStar className='fill-amber-500'/>}
+        {post.rating >= 4 ? <BsStarFill className='fill-amber-400'/>:<BsStar className='fill-amber-500'/>}
+        {post.rating >= 5 ? <BsStarFill className='fill-amber-400'/>:<BsStar className='fill-amber-500'/>}
       </div>
       <h1>{post.post}</h1>
       <div className="flex justify-between">
