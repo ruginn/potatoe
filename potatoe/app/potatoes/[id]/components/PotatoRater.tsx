cc2cc2ad598b7aa6755b574d8b9e81335f29ffe1
Rcatarata <Rcatarata@gmail.com>
2023-10-16 20:09:55 -0400

    changed post page to reload and set to blank

--- potatoe/app/potatoes/[id]/components/PotatoRater.tsx
+++ potatoe/app/potatoes/[id]/components/PotatoRater.tsx
@@ -5,6 +5,7 @@ import {BsStarFill, BsStar } from 'react-icons/bs'
 import { getServerSession } from "next-auth";
 import { authOptions } from '../../../api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 // async function createPost(data: FormData) {
 //   'use server'
@@ -21,10 +22,15 @@ import { redirect } from 'next/navigation';
 // }
 
 const PotatoRater = ({potato}) => {
-
+  const router = useRouter()
   const [rate, setRate] = useState({})
   const [starRating, setStarRating] = useState(0)
+  const [post, setPost] = useState('')
   
+  const handlePostChange = (e) => {
+    setPost(e.target.value)
+  }
+
   const createPost = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
@@ -43,7 +49,8 @@ const PotatoRater = ({potato}) => {
       } ,
     })
     await res.json()
-    
+    setPost('')
+    router.refresh()
   }
 
 
@@ -52,7 +59,7 @@ const PotatoRater = ({potato}) => {
         <h1>Rate Potato</h1>
         <form onSubmit={createPost} className='flex flex-col w-[500px]'>
             <label htmlFor="post">Tell us what you think about {potato.name}</label>
-            <input type="text" id="post" name='post' className='pl-1 border border-slate-300 rounded-md'/>
+            <input type="text" id="post" name='post' value={post} onChange={handlePostChange} className='pl-1 border border-slate-300 rounded-md'/>
             <div className='flex flex-row cursor-pointer'>
                 {starRating >= 1 ? <BsStarFill onClick={() => setStarRating(1)} /> : <BsStar onClick={() => setStarRating(1)} />}
                 {starRating >= 2 ? <BsStarFill onClick={() => setStarRating(2)}/> : <BsStar onClick={() => setStarRating(2)}/>}
