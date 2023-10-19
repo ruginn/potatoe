c8319ac54240688227294b6cbb09a9b048d7176c
Rcatarata <Rcatarata@gmail.com>
2023-10-19 09:34:17 -0400

    hero

---
+++ potatoe/app/component/PotatoHero.tsx
@@ -0,0 +1,70 @@
+'use client'
+import {motion, useScroll, useTransform} from 'framer-motion'
+import Potato from '../../public/assets/potatoe.png'
+import Image from 'next/image'
+import particleTexture from '../../public/assets/backgrounds/particleTexture.jpg'
+import {useRef} from 'react'
+
+function PotatoHero() {
+    const ref = useRef(null)
+    const {scrollYProgress} = useScroll({
+        target: ref, 
+        offset: ["0 1", "1.33, 1"],
+    })
+
+    const x1 = useTransform(scrollYProgress, [0,1], [-40000, 700])
+  return (
+    <div className='w-screen relative h-[80vh]'>
+        <div className='w-screen h-full absolute z-1'>
+            <Image 
+                src={particleTexture}
+                alt=''
+                className='w-screen h-full object-cover'
+            ></Image>
+        </div>
+        <div className='bg-gradient-to-tr from-amber-300 via-yellow-100 to-amber-200 w-full h-full absolute z-2 opacity-80 flex justify-center items-center'>
+            <div className='flex flex-col h-full justify-center items-center relative'>
+            <motion.div
+                initial={{ rotate: 45, }}
+                // whileInView={{x:1}}
+                // transition={{
+                //   duration: 2,
+                //   ease: "easeInOut",
+                //   // repeat: Infinity,
+                //   // repeatDelay: 1
+                // }}
+                style={{
+                  // scale: scrollYProgress, 
+                  // opacity: scrollYProgress, 
+                  x: x1
+                }}
+                className='absolute'
+              >
+                <Image 
+                  ref={ref}
+                  src={Potato}
+                  alt=''
+                  height={700}
+                  className='drop-shadow-2xl m-0'
+                ></Image>
+            </motion.div>
+            <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] font-bold m-0'>POTATO</h1>
+            <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] font-bold'>POTATO</h1>
+            <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] font-bold'>POTATO</h1>
+            <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] font-bold'>POTATO</h1>
+            <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] font-bold'>POTATO</h1>
+            <div className='absolute'>
+              <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] text-transparent font-bold outline--text--2 m-0'>POTATO</h1>
+              <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] text-transparent font-bold outline--text--2'>POTATO</h1>
+              <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] text-transparent font-bold outline--text--2'>POTATO</h1>
+              <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] text-transparent font-bold outline--text--2'>POTATO</h1>
+              <h1 className='text-7xl lg:text-[9rem] leading-none tracking-[.3em] text-transparent font-bold outline--text--2'>POTATO</h1>
+            </div>
+        </div>
+        </div>
+        <p>testing</p>
+    </div>
+  )
+}
+
+export default PotatoHero
--- potatoe/app/page.tsx
+++ potatoe/app/page.tsx
@@ -1,6 +1,7 @@
 import PotatoeBanner from "./component/PotatoBanner"
 import PotatoeGrid from "./component/PotatoGrid"
 import Hero from "./component/hero"
+import PotatoHero from "./component/PotatoHero"
 import Goop from "./component/Goop"
 import CardSlider from "./component/CardSlider"
 import PotatoeFactsBanner from "./component/PotatoFactsBanner"
@@ -10,7 +11,8 @@ export default function Home() {
 
   return (
     <main >
-      <Hero />
+      <PotatoHero />
+      <Hero /> 
       <PotatoeGrid />
       {/* <PotatoeBanner /> */}
       {/* <Goop /> */}
